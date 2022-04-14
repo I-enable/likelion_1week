4fe0e539d0928ebe9d6cc41d201a4acc68964496
unknown <tlawogud12@naver.com>
2022-04-14 16:39:49 +0900

    code_review_update

--- datakill.js
+++ datakill.js
@@ -1,24 +1,21 @@
+let frogChange = document.querySelectorAll(".frog");
 
-if (localStorage.getItem("kill") == 0) {
-    document.querySelector(".main_face").src="그런건없다.PNG";
-    let frogchange = document.querySelectorAll(".frog");
-    frogchange[0].src="그런건없다.PNG";
-    frogchange[1].src="그런건없다.PNG";
-    frogchange[2].src="그런건없다.PNG";
+function joke() {
+    document.querySelector(".mainFace").src="그런건없다.PNG";
+    frogChange[0].src="그런건없다.PNG";
+    frogChange[1].src="그런건없다.PNG";
+    frogChange[2].src="그런건없다.PNG";
     document.querySelector("#title").innerText="No Such Thing ㅋㅋ";
-    document.querySelector(".pos_text").innerText="응 새로고침 해도 안돼 ㅋㅋ";
+    document.querySelector(".posText").innerText="응 새로고침 해도 안돼 ㅋㅋ";
 }
 
-function datakill() {
+if (localStorage.getItem("kill") == 0) {
+    joke();
+}
+
+function dataKill() {
     localStorage.setItem("kill", 0);
     if (localStorage.getItem("kill") == 0) {
-        document.querySelector(".main_face").src="그런건없다.PNG";
-        document.querySelectorAll(".frog").src="그런건없다.PNG";
-        let frogchange = document.querySelectorAll(".frog");
-        frogchange[0].src="그런건없다.PNG";
-        frogchange[1].src="그런건없다.PNG";
-        frogchange[2].src="그런건없다.PNG";
-        document.querySelector("#title").innerText="No Such Thing ㅋㅋ";
-        document.querySelector(".pos_text").innerText="응 새로고침 해도 안돼 ㅋㅋ";
+        joke();
     }
 }
